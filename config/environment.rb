0e1d8dee76525a2b35828254a62ef5f0a60bcfb3
Andy Cobley <acobley@computing.dundee.ac.uk>
2011-06-27 13:44:05 +0000

    yet more files

---
+++ config/environment.rb
@@ -0,0 +1,13 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+Multipage::Application.initialize!
+
+# Ensure the gateway is in test mode
+ ActiveMerchant::Billing::Base.gateway_mode = :test
+ ActiveMerchant::Billing::Base.integration_mode = :test
+ ActiveMerchant::Billing::PaypalGateway.pem_file =
+                 File.read(File.dirname(__FILE__) + '/../paypal/paypal_cert.pem')
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Multipage::Application.config.secret_token = '08da6f7b588eccb94095ada6c45c1c31f9314bfee926c169c121cec4b825357b448bff5197747724cc1a2eed519bfa12cb8750fe8c591203248609f3909abcb5'
